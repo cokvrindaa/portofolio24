aa2045051b786e251cfcfc3deb2956d2a242cce1
master.cokorda@gmail.com <master.cokorda@gmail.com>
2024-09-27 13:21:43 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function iglink() {
+  location.href = "https://www.instagram.com/cokvrindaaa88/";
+}
+function tiktoklink() {
+  location.href = "https://www.tiktok.com/@cokvrindaa";
+}
+function githublink() {
+  location.href = "https://github.com/cokvrindaa";
+}
+var typed = new Typed("#typed-name", {
+  strings: ["Cok Vrinda"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
+// Logo coursel
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
+// 2
+var copy1 = document.querySelector(".logos-slide1").cloneNode(true);
+document.querySelector(".logos1").appendChild(copy1);
+// Slider js
+
